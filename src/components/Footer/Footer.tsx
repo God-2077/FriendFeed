73140ab4324d6277678947b9d2f189280b5e4c4c
kissablecho <god_2077@outlook.com>
2026-04-15 18:17:50 +0800

    fix: 修复页脚 astro-koharu 链接

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -12,7 +12,7 @@ const Footer: FC<FooterProps> = () => {
   return (
     <footer className="page-footer">
       <p>
-        Powered by <a href='https://github.com/God-2077/FriendFeed' target="_blank" rel="noopener noreferrer">FriendFeed</a> · Inspired by <a href="https://github.com/amehime/hexo-theme-shoka" target="_blank" rel="noopener noreferrer">astro-koharu</a> ·  Designed with <RiHeart3Fill size={16} color="currentColor" className="heart-beat" /> by <a href="https://github.com/kissablecho" target="_blank" rel="noopener noreferrer">kissablecho</a>
+        Powered by <a href='https://github.com/God-2077/FriendFeed' target="_blank" rel="noopener noreferrer">FriendFeed</a> · Inspired by <a href="https://github.com/cosZone/astro-koharu" target="_blank" rel="noopener noreferrer">astro-koharu</a> ·  Designed with <RiHeart3Fill size={16} color="currentColor" className="heart-beat" /> by <a href="https://github.com/kissablecho" target="_blank" rel="noopener noreferrer">kissablecho</a>
       </p>
       <p className="copyright">
         <a href="https://aboutfeeds.com" target="_blank" rel="noopener noreferrer">
