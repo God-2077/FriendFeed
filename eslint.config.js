fac6c88e33f8e8b66233b1fff1673045861336a2
Kissablecho <god_2077@outlook.com>
2026-04-18 13:32:58 +0800

    build: 更新 ESLint 配置及相关依赖
    移除旧的 ESLint 插件和配置，添加 @typescript-eslint/parser 和 eslint-plugin-astro 依赖
    简化 eslint.config.js 配置，使用 astro 推荐的规则集

--- eslint.config.js
+++ eslint.config.js
@@ -1,23 +1,12 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
-import { defineConfig, globalIgnores } from 'eslint/config'
-
-export default defineConfig([
-  globalIgnores(['dist']),
+import eslintPluginAstro from 'eslint-plugin-astro';
+export default [
+  // add more generic rule sets here, such as:
+  // js.configs.recommended,
+  ...eslintPluginAstro.configs.recommended,
   {
-    files: ['**/*.{ts,tsx}'],
-    extends: [
-      js.configs.recommended,
-      tseslint.configs.recommended,
-      reactHooks.configs.flat.recommended,
-      reactRefresh.configs.vite,
-    ],
-    languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
-    },
-  },
-])
+    rules: {
+      // override/add rules settings here, such as:
+      // "astro/no-set-html-directive": "error"
+    }
+  }
+];
